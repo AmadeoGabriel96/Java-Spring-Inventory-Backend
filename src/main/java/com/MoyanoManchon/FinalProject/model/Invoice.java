61ff26b8e84b12d536a54792df21f93723ffb375
AmadeoGabriel96 <129233416+AmadeoGabriel96@users.noreply.github.com>
2026-04-12 22:35:49 -0300

    Initialize invoiceDetailsList and add createdAt field
    Initialize invoiceDetailsList and add createdAt field.

--- src/main/java/com/MoyanoManchon/FinalProject/model/Invoice.java
+++ src/main/java/com/MoyanoManchon/FinalProject/model/Invoice.java
@@ -2,9 +2,12 @@ package com.MoyanoManchon.FinalProject.model;
 
 import lombok.Data;
 
+
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Data
 @Entity
@@ -21,7 +24,12 @@ public class Invoice {
     private Client client;
 
     @OneToMany(mappedBy = "invoice", cascade = CascadeType.ALL)
-    private List<InvoiceDetails> invoiceDetailsList;
+    private List<InvoiceDetails> invoiceDetailsList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "invoice", cascade = CascadeType.ALL, orphanRemoval = true)
+
+    @CreationTimestamp
+    private Date createdAt;
 
 }
 
